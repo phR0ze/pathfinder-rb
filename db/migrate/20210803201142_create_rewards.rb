0077391525f011e4eeeb350d4149c4eaf9f743ca
phR0ze <prc451@gmail.com>
2022-06-29 19:23:00 -0600

    Adding database updates

---
+++ db/migrate/20210803201142_create_rewards.rb
@@ -0,0 +1,10 @@
+class CreateRewards < ActiveRecord::Migration[6.1]
+  def change
+    create_table :rewards do |t|
+      t.integer :value
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_30_010705) do
+ActiveRecord::Schema.define(version: 2021_08_03_201142) do
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "points", force: :cascade do |t|
     t.integer "value"
     t.integer "user_id", null: false
     t.integer "category_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_points_on_category_id"
     t.index ["user_id"], name: "index_points_on_user_id"
   end
@@ -31,15 +32,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_30_010705) do
   create_table "rewards", force: :cascade do |t|
     t.integer "value"
     t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_rewards_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "points", "categories"
